b7ec6d0595341bc80b4f9252a183a45b79f012a4
Roman Indermühle <roman.indermuehle@icloud.com>
2023-10-11 17:24:42 +0200

    completely rebuild the calc. logic

--- UnitConversions/ContentView.swift
+++ UnitConversions/ContentView.swift
@@ -8,56 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var inputNumber = ""
-    @State private var result = ""
+    @State private var inputValue = 0.0
     
-    @ObservedObject var viewModel = ContentViewModel()
+    @State var inputUnit: Temperature = .Celsius
+    @State var outputUnit: Temperature = .Celsius
     
     var body: some View {
-        NavigationView {
+//        Self._printChanges()
+       NavigationView {
             Form {
                 Section {
-                    Text("Select units")
+                    Text("Select input unit")
                         .font(.headline)
-                    Picker("Convert", selection: $viewModel.inputUnit) {
+                    Picker("Convert", selection: $inputUnit) {
                         ForEach(Temperature.allCases, id: \.self) { temperature in
                             Text(temperature.rawValue)
                         }
-                    }
-                    Picker("To", selection: $viewModel.outputUnit) {
+                    }.pickerStyle(.segmented)
+                    Text("Select output unit")
+                        .font(.headline)
+                    Picker("To", selection: $outputUnit) {
                         ForEach(Temperature.allCases, id: \.self) { temperature in
                             Text(temperature.rawValue)
                         }
-                    }
+                    }.pickerStyle(.segmented)
                 }
                 Section {
                     Text("Value")
                         .font(.headline)
                     HStack {
-                        TextField("Enter value", text: $inputNumber)
-                            .onChange(of: inputNumber) {
-                                result = viewModel.convertUnit(number: Double(inputNumber) ?? 0)
-                                print(result)
-                            }
-                            .onChange(of: viewModel.inputUnit) {
-                                result = viewModel.convertUnit(number: Double(inputNumber) ?? 0)
-                                print(result)
-                            }
-                            .onChange(of: viewModel.outputUnit) {
-                                result = viewModel.convertUnit(number: Double(inputNumber) ?? 0)
-                                print(result)
-                            }
+                        TextField("Enter value", value: $inputValue, format: .number)
                             .keyboardType(.decimalPad)
                     }
                 }
                 Section {
                     Text("Result")
                         .font(.headline)
-                    Text("\(result)")
+                    Text(String(format: "%.2f", outputValue))
                 }
                 
             }
-            .navigationTitle("Unit Conversion")
+            .navigationTitle("Unit Conversion 🥶")
+        }
+    }
+    
+    enum Temperature: String, CaseIterable {
+        case Celsius
+        case Fahrenheit
+        case Kelvin
+    }
+    
+    var inputInCelsius: Double {
+        switch inputUnit {
+        case .Celsius:
+            return inputValue
+        case .Fahrenheit:
+            return (inputValue - 32) * 5/9
+        case .Kelvin:
+            return inputValue - 273.15
+        }
+    }
+    
+    var outputValue: Double {
+        switch outputUnit {
+        case .Celsius:
+            return inputInCelsius
+        case .Fahrenheit:
+            return (inputInCelsius * 9/5) + 32
+        case .Kelvin:
+            return inputInCelsius + 273.15
+            
         }
     }
 }
